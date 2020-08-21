2ac15f786ac293b3fdb042922b68b44fc41425d1
Fahsaaula <63630169+Fahsaaula@users.noreply.github.com>
2020-08-22 04:58:11 +0700

    Upload file classcoba uji coba class

---
+++ classcoba.php
@@ -0,0 +1,20 @@
+<?php 
+
+//membuat sebuah class
+class uji {
+
+
+}
+
+
+//membuat beberapa object
+$object1 = new uji();
+$object2 = new uji();
+
+//menampilkan nilai
+
+var_dump($object1);
+var_dump($object2);
+
+
+ ?>
